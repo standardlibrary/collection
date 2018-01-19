1440ce0043289e5f8ada80ac6c587bf06ba153fb
unwarysheep <unwarysheep@icloud.com>
2018-01-19 15:13:48 +0000

    Fix modulus

--- tests/Collection/Methods/FilterTest.php
+++ tests/Collection/Methods/FilterTest.php
@@ -51,7 +51,7 @@ final class FilterTest extends TestCase
 
                 // Modifying function
                 function(int $value, array $args) {
-                    return ($value % $args['divisor'] === 2) ? true : false;
+                    return ($value % $args['divisor'] === 0) ? true : false;
                 },
 
                 [ 'divisor' => 2 ], // Array of arguments
